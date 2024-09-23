2eb58bea8c870f6f03b8cb6f69e1ac0e72642d35
Rishikeshari7 <rishikeshari7@gmail.com>
2024-09-23 15:13:05 +0530

    added redirect to whatsapp feature

--- src/components/homepage/ImageBgSection.js
+++ src/components/homepage/ImageBgSection.js
@@ -2,17 +2,40 @@ import React from "react";
 import Image from "next/image";
 import Heroimage from "../../../public/Heroimage.jpg";
 // import { counselorOptions } from "@/data/counselorOptions";
-import { FaClipboardCheck, FaCalendarAlt } from 'react-icons/fa';
+import { FaClipboardCheck, FaCalendarAlt } from "react-icons/fa";
 import { BsWhatsapp } from "react-icons/bs";
 import { MdOutlinePhoneInTalk } from "react-icons/md";
+import Link from "next/link";
 
 const ImageBgSection = () => {
   const counselorOptions = [
-    { id: 1, name: "Chat with Counsellor", icon: <BsWhatsapp  />,bgColor:"bg-pink-400" },
-    { id: 2, name: "Talk to Psychologist", icon: <MdOutlinePhoneInTalk />,bgColor:"bg-teal-400" },
-    { id: 3, name: "Get a Consultation", icon: <FaClipboardCheck />,bgColor:"bg-blue-400" },
-    { id: 4, name: "Book an Appointment", icon: <FaCalendarAlt />,bgColor:"bg-orange-400" },
+    {
+      id: 1,
+      name: "Chat with Counsellor",
+      icon: <BsWhatsapp />,
+      bgColor: "bg-pink-400",
+    },
+    {
+      id: 2,
+      name: "Talk to Psychologist",
+      icon: <MdOutlinePhoneInTalk />,
+      bgColor: "bg-teal-400",
+    },
+    {
+      id: 3,
+      name: "Get a Consultation",
+      icon: <FaClipboardCheck />,
+      bgColor: "bg-blue-400",
+    },
+    {
+      id: 4,
+      name: "Book an Appointment",
+      icon: <FaCalendarAlt />,
+      bgColor: "bg-orange-400",
+    },
   ];
+  const whatsappNumber = "9260960495";
+  const message = "Hello! I'd like to know more about your services.";
   return (
     <div className=" bg-customLightgreen py-10 flex justify-center items-center flex-1 overflow-clip ">
       <div className="flex flex-col justify-center items-center w-11/12 mx-auto gap-5 overflow-hidden ">
@@ -30,9 +53,16 @@ const ImageBgSection = () => {
               thrive. Our compassionate support is designed to guide you through
               your struggles, whether it’s overcoming stress, managing anxiety.
             </p>
-            <button className="bg-white text-customGreen font-semibold px-5 py-2 rounded-full w-fit ">
-              Chat Now
-            </button>
+            <Link
+              href={`https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+                message
+              )}`}
+              target="_blank"
+            >
+              <button className="bg-white hover:bg-customLightgreen transform duration-300 hover:text-white text-customGreen font-semibold px-5 py-2 rounded-full w-fit ">
+                Chat Now
+              </button>
+            </Link>
           </div>
         </div>
 
@@ -45,7 +75,9 @@ const ImageBgSection = () => {
               <div
                 className={`flex justify-center items-center ${data.bgColor} rounded-full w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28 `}
               >
-                <span className=" text-3xl sm:text-6xl text-white">{data.icon}</span>
+                <span className=" text-3xl sm:text-6xl text-white">
+                  {data.icon}
+                </span>
               </div>
               <p className="font-semibold text-center text-sm sm:text-lg text-customGreen mx-1 sm:mt-4">
                 {data.name}
