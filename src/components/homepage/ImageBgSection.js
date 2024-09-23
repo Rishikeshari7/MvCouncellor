9ec6b7ca0b1a4f3b4db723d022e203f24730d3df
Rishikeshari7 <rishikeshari7@gmail.com>
2024-09-23 23:32:43 +0530

    added services section

--- src/components/homepage/ImageBgSection.js
+++ src/components/homepage/ImageBgSection.js
@@ -6,6 +6,7 @@ import { FaClipboardCheck, FaCalendarAlt } from "react-icons/fa";
 import { BsWhatsapp } from "react-icons/bs";
 import { MdOutlinePhoneInTalk } from "react-icons/md";
 import Link from "next/link";
+import { services } from "@/data/services";
 
 const ImageBgSection = () => {
   const counselorOptions = [
@@ -37,7 +38,7 @@ const ImageBgSection = () => {
   const whatsappNumber = "9260960495";
   const message = "Hello! I'd like to know more about your services.";
   return (
-    <div className=" bg-customLightgreen py-10 flex justify-center items-center flex-1 overflow-clip ">
+    <div className=" bg-customLightgreen py-10 flex flex-col justify-center items-center flex-1 overflow-clip ">
       <div className="flex flex-col justify-center items-center w-10/12 mx-auto gap-5 overflow-hidden ">
         <div className="relative">
           <div className="overflow-clip rounded-2xl ">
@@ -70,7 +71,7 @@ const ImageBgSection = () => {
           {counselorOptions.map((data) => (
             <div
               key={data.id}
-              className="bg-white mx-auto flex py-3 sm:p-5 flex-col justify-between items-center rounded-2xl w-36 h-32  sm:w-48 sm:h-52  md:w-60 md:h-48"
+              className="bg-white shadow-2xl mx-auto flex py-3 sm:p-5 flex-col justify-between items-center rounded-2xl w-36 h-32  sm:w-48 sm:h-52  md:w-60 md:h-48"
             >
               <div
                 className={`flex justify-center items-center ${data.bgColor} rounded-full w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28 `}
@@ -85,6 +86,33 @@ const ImageBgSection = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className="flex flex-col gap-6 sm:gap-10 justify-center w-full my-10 sm:my-20 py-8 sm:py-14 items-center flex-1 bg-[#dee3e0] ">
+      <p className=" text-xl sm:text-4xl font-bold text-customGreen" >Your Path to Mental Wellness</p>
+      <div className=" w-10/12 flex flex-col justify-center items-center mx-auto gap-5">
+        <div className=" grid grid-cols-2 sm:flex w-full flex-wrap gap-4 items-stretch justify-between">
+          {services.map((data) => (
+            <div
+              key={data.id}
+              className="bg-white gap-1 shadow-2xl mx-auto flex py-3 sm:p-5 flex-col justify-between items-center rounded-2xl w-36 h-58  sm:w-64 sm:h-80  md:w-64 md:h-80"
+            >
+              <div
+                className={`flex justify-center items-center bg-customGreen rounded-full w-14 h-14 sm:w-24 sm:h-24 md:w-28 md:h-28 `}
+              >
+                <span className=" text-3xl sm:text-6xl aspect-square   text-white">
+                  {data.icon}
+                </span>
+              </div>
+              <p className="font-bold text-center text-sm sm:text-xl text-customGreen mx-1 sm:mt-4">
+                {data.title}
+              </p>
+              <p className="font-medium text-center text-sm sm:text-lg text-customGreen2 mx-1 sm:mt-4">
+                {data.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
       </div>
     </div>
   );
