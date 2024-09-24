f0d8463487da51ef0222fb4364088c251569c068
Rishikeshari7 <rishikeshari7@gmail.com>
2024-09-24 18:55:07 +0530

    adjust size of image homepage

--- src/components/homepage/PsychologistAndCustomers.js
+++ src/components/homepage/PsychologistAndCustomers.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const PsychologistAndCustomers = () => {
   return (
-    <div className='h-10' >
-      {/* PsychologistAndCustomers */}
+    <div className="flex justify-center items-center">
+      <div className="flex w-10/12 justify-center items-center border-2 border-black my-10 rounded-lg">
+      
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PsychologistAndCustomers
+export default PsychologistAndCustomers;
